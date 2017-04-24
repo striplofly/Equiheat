535655a19bee64e21e274dfc2d183bc39cfb0df2
striplofly <fatinnurliyana8@gmail.com>
2017-04-24 09:40:24 +0800

    Add Joystick function

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public VirtualJoyStick joyStick;
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+
+    private Vector3 moveDirection;
+    private float xMin, xMax, yMin, yMax;
+
+	void Start ()
+    {
+        //Initialization of boundaries
+        xMax = Screen.width - 50; // I used 50 because the size of player is 100*100
+        xMin = 50;
+        yMax = Screen.height - 50;
+        yMin = 50;
+    }
+	
+	void Update ()
+    {
+        moveDirection = joyStick.inputVector;
+
+        if(moveDirection.magnitude != 0)
+        {
+            transform.position += moveDirection * moveSpeed;
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);
+        }
+    }
+
+    /*Vector3 UpdateJoyStickPos()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = joyStick.Horizontal();
+        direction.z = joyStick.Vertical();
+
+        if (direction.magnitude > 1)
+            direction.Normalize();
+
+        return direction;
+    }
+
+    void UpdatePlayerPos()
+    {
+        playerRigidBody.AddForce(moveVector * moveSpeed);
+    }*/
+}
---
+++ Assets/Scripts/VirtualJoyStick.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class VirtualJoyStick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public Vector3 inputVector;
+
+    private Image bgJoyStick;
+    private Image joyStick;
+
+    void Start()
+    {
+        bgJoyStick = GetComponent<Image>();
+        joyStick = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(bgJoyStick.rectTransform, ped.position, ped.pressEventCamera, out pos);
+        {
+            // ratio mouse pointer with bgJoyStick -> Quad4
+            pos.x = pos.x / bgJoyStick.rectTransform.sizeDelta.x;
+            pos.y = pos.y / bgJoyStick.rectTransform.sizeDelta.y;
+
+            // Quad 4 -> Quad1
+            float x = (bgJoyStick.rectTransform.pivot.x == 1f) ? pos.x * 2 + 1 : pos.x * 2 - 1;
+            float y = (bgJoyStick.rectTransform.pivot.y == 1f) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+
+            inputVector = new Vector3(x, y, 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            // update joyStick pos + define area joystick
+            joyStick.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgJoyStick.rectTransform.sizeDelta.x/3), inputVector.y * (bgJoyStick.rectTransform.sizeDelta.y/3));
+        }
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        joyStick.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
